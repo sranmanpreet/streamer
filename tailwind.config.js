eac717185447e10109046517083b104030f5201f
Sran Manpreet <manna1661@gmail.com>
2023-05-16 05:23:34 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '370px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
